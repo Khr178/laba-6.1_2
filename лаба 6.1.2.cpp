8a17249413df06a87f7766f5034020d82a3dc0ef
Khrystyna <hrustunakopeika2005@gmail.com>
2022-11-17 15:26:17 +0200

    first master commit

---
+++ лаба 6.1.2.cpp
@@ -0,0 +1,86 @@
+﻿
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+
+using namespace std;
+int main()
+{
+}
+void mas(int*b, int arr[], const int size, const int min, const int max)
+{
+    
+    for (int i = 0; i < size; i++) 
+    {
+        int range = max - min + 1;
+        arr[i] = rand() % range + min;
+    }
+}
+void print_array(int arr[], int size) 
+{
+    for (int i = 0; i < size; i++) 
+    {
+        std::cout << arr[i] << std::setw(4);
+    }
+    std::cout << std::endl;
+}
+
+bool is_valid(int n) 
+{
+    if ((n % 2 == 0) && (n < 0)) 
+    {
+        return true;
+    }
+    else 
+    {
+        return false;
+    }
+}
+
+int count_quantity(int arr[], int size) {
+    int quantity = 0;
+
+    for (int i = 0; i < size; i++) {
+        if (is_valid(arr[i])) {
+            ++quantity;
+        }
+    }
+    return quantity;
+}
+
+int sum(int arr[], const int size) 
+{
+    int sum = 0;
+
+    for (int i = 0; i < size; i++) 
+    {
+        if (is_valid(arr[i])) 
+        {
+            sum += arr[i];
+        }
+    }
+    return sum;
+}
+
+void mas2(int arr[], int size) 
+{
+    for (int i = 0; i < size; i++) 
+    {
+        if (is_valid(arr[i])) 
+        {
+            arr[i] = 0;
+        }
+    }
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ юніт тест 6.1.2.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <cassert>
+#include <iostream>
+#include "../лаба 6.1.2/лаба 6.1.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace юніттест612
+{
+	TEST_CLASS(юніттест612)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int b;
+			b = main();
+			Assert::AreEqual(b, 0);
+		}
+	};
+}
+
+
+
